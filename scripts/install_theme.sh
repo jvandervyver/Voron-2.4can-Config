ac66112ef858b6b1136ed62a1c9a37df35bf9ca7
Johan van der Vyver <github.com@johan.vdvyver.com>
2023-05-07 20:33:47 +0200

    Add theme scripts

---
+++ scripts/install_theme.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+THEME_DIRECTORY="${home}/printer_data/config/.theme"
+
+mkdir "${THEME_DIRECTORY}"
+
+wget 'https://raw.githubusercontent.com/jvandervyver/Voron-2.4can-Config/main/downloads/voron_logos/favicon-16x16.png' -O "${THEME_DIRECTORY}/favicon-16x16.png"
+wget 'https://raw.githubusercontent.com/jvandervyver/Voron-2.4can-Config/main/downloads/voron_logos/favicon-32x32.png' -O "${THEME_DIRECTORY}/favicon-32x32.png"
+wget 'https://raw.githubusercontent.com/jvandervyver/Voron-2.4can-Config/main/downloads/voron_logos/sidebar-logo.png' -O "${THEME_DIRECTORY}/sidebar-logo.png"
